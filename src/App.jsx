0214d29855fbf43f84a942724ad549ad828911e5
Anderson Velásquez <andersonjosvela@gmail.com>
2024-12-28 16:41:52 -0600

    Se agrego la logica de las rutas al archivo principal para que se pueda renderizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import './App.css'
+import './index.css'
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage.jsx'
-import Dashboard from './pages/Dashboard.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import Login from './pages/LoginPage.jsx'
+import HomeCliente from './pages/HomeCliente.jsx'
+import HomeOperador from './pages/HomeOperador.jsx'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LoginPage/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/cliente' element={
+            <ProtectedRoute roles={[2]}>
+              <HomeCliente />
+            </ProtectedRoute>
+          } />
+          <Route path='/operador' element={
+            <ProtectedRoute roles={[1]}>
+              <HomeOperador />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
